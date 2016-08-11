ad275ccd9568653164b95e45b59cf8d3ca8c4e54
olamedrek <medrekaleksandra@gmail.com>
2016-08-11 19:51:52 +0200

    Board logic

---
+++ app/src/main/java/com/ola/boggle/Board.java
@@ -0,0 +1,48 @@
+package com.ola.boggle;
+
+import java.util.Random;
+
+/**
+ * Created by ola on 11.08.16.
+ */
+public class Board {
+
+    private final Character[] letters = {'A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G',
+            'H', 'I', 'J', 'K', 'L', 'Ł', 'M', 'N', 'Ń', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'Ś', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z', 'Ź', 'Ż'};
+
+    private final double[] freq = {0.0891, 0.0099, 0.0147, 0.0396, 0.0040, 0.0325,
+            0.0766, 0.0111, 0.0030, 0.0142, 0.0108, 0.0821, 0.0228, 0.0351, 0.0210, 0.0182,
+            0.0280, 0.0552, 0.0020, 0.0775, 0.0085, 0.0313, 0.0014, 0.0469, 0.0432, 0.0066,
+            0.0398, 0.0250, 0.0004, 0.0465, 0.0002, 0.0376, 0.0564, 0.0006, 0.0083};
+
+    private double[] freqPref = new double[freq.length];
+
+    private Random random = new Random();
+
+    public Board() {
+        freqPref[0] = freq[0];
+        for(int i = 1; i < freq.length; i++) {
+            freqPref[i] = freqPref[i-1] + freq[i];
+        }
+    }
+
+    public String getNewBoard() {
+        StringBuilder builder = new StringBuilder();
+        while(builder.length() < 16) {
+            int i = getRandomIndex();
+            if(i != -1)
+                builder.append(letters[i]);
+        }
+        return builder.toString();
+    }
+
+    private int getRandomIndex() {
+        Double d = random.nextDouble();
+        for(int i = 0; i < freqPref.length; i++) {
+            if(freqPref[i] > d) return i;
+        }
+        return -1;
+    }
+
+}
--- app/src/main/java/com/ola/boggle/MainActivity.java
+++ app/src/main/java/com/ola/boggle/MainActivity.java
@@ -3,7 +3,9 @@ package com.ola.boggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     private List<TextView> letterViews = new ArrayList<>();
     private RelativeLayout boardLayout;
+    private Board board = new Board();
+    private Button newButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         boardLayout = (RelativeLayout) findViewById(R.id.board_layout);
+        newButton = (Button) findViewById(R.id.new_button);
 
         letterViews.add((TextView) findViewById(R.id.letter_view1));
         letterViews.add((TextView) findViewById(R.id.letter_view2));
@@ -38,6 +43,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        newButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = board.getNewBoard();
+                for(int i = 0; i < letterViews.size(); i++) {
+                    letterViews.get(i).setText(String.valueOf(text.charAt(i)));
+                }
+            }
+        });
+
+
+
 
 
     }
